2898fafe8bb7c6167dde221c5cc330288a1f219e
Angelo Santos <angelo.santos@foster.com.br>
2018-07-20 10:29:44 -0300

    Plugin info

---
+++ wp-multiple-domains.php
@@ -0,0 +1,11 @@
+<?php 
+
+/*
+Plugin Name: WP Multiple Domains
+Plugin URI: https://github.com/angelosds/wp-multiple-domains
+Description: A plugin to allow wordpress development with multiple domains 🤖
+Version: 1.0.0
+Author: Angelo Santos
+Author URI: https://github.com/angelosds
+License: MIT
+*/
