6ad8567afee1d52bc2b1ecd8b12b73e17e397a0a
Angelo Santos <angelo.santos@foster.com.br>
2018-07-20 12:05:27 -0300

    Replace multiple domains by actual and prevent siteurl redirect

---
+++ domains.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Add all domains to AMBIENT_DOMAINS array
+ */
+
+define('AMBIENT_DOMAINS', array(
+    'http://localhost:4020',
+    'http://localhost:4021'
+));
--- wp-multiple-domains.php
+++ wp-multiple-domains.php
@@ -9,3 +9,61 @@ Author: Angelo Santos
 Author URI: https://github.com/angelosds
 License: MIT
 */
+
+class MultipleDomains {
+    private $file_url;
+    private $config_url;
+    
+    function __construct() {
+        $file_name = 'domains.php';
+        $this->file_url = plugin_dir_path(__FILE__) . $file_name;
+        $this->config_url = ABSPATH . 'wp-content/uploads/' . $file_name;
+
+        $this->init();
+    }
+
+    /**
+     * Set current URL as home and siteurl
+     */
+    public function init() {
+        $this->add_config_file();
+
+        define('AMBIENT_DOMAIN', $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST']);
+
+        echo AMBIENT_DOMAIN;
+        print_r( AMBIENT_DOMAINS );
+
+        define('WP_HOME', AMBIENT_DOMAIN);
+        define('WP_SITEURL', AMBIENT_DOMAIN);
+    }
+
+    /**
+     * Copy domains file to uploads folder
+     */
+    private function add_config_file() {
+        if (!file_exists($this->config_url)) {
+            echo copy($this->file_url, $this->config_url);
+        }
+
+        require_once $this->config_url;
+    }
+}
+
+add_action('after_setup_theme', 'setup');
+
+function setup() {
+    $controller = new MultipleDomains();
+    
+    add_action('shutdown', 'buffer_end');
+
+    ob_start('buffer_start');
+}
+
+function buffer_start($buffer) {
+    $buffer = str_replace(AMBIENT_DOMAINS, AMBIENT_DOMAIN, $buffer);
+    return $buffer; 
+}
+
+function buffer_end() {
+    ob_end_flush();
+}
