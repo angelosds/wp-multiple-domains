0472264e274e799be4dec79368de29424121d3a0
Angelo Santos <angelo.santos@foster.com.br>
2018-07-27 11:58:14 -0300

    Moved filters to MultipleDomains class

--- wp-multiple-domains.php
+++ wp-multiple-domains.php
@@ -26,9 +26,55 @@ class MultipleDomains {
         define('WP_HOME', AMBIENT_DOMAIN);
         define('WP_SITEURL', AMBIENT_DOMAIN);
 
-        ob_start('buffer_start');
+        // Rendered HTML
+        ob_start([$this, 'buffer_start']);
+        add_action('shutdown', [$this, 'buffer_end']);
 
-        add_action('shutdown', 'buffer_end');
+        // Replace domain in certain wordpress filters
+        add_filter('wp_get_attachment_url', [$this, 'media_gallery']);
+        add_filter('wp_prepare_themes_for_js', [$this, 'themes']);
+        add_filter('admin_post_thumbnail_html', [$this, 'thumbnail']);
+    }
+
+    /**
+     * Replace domain ocorrences in rendered HTML
+     */
+    function buffer_start($buffer) {
+        return $this->replace_string($buffer);
+    }
+    
+    /**
+     * Apply new html content with correct domain name
+     */
+    function buffer_end() {
+        ob_end_flush();
+    }
+
+    /**
+     * Replace domain in media gallery
+     */
+    function media_gallery($url) {
+        return $this->replace_string($url);
+    }
+
+    /**
+     * Replace domain in themes gallery
+     */
+    function themes($themes) {
+        foreach ($themes as $theme_index => $theme) {
+            foreach ($theme['screenshot'] as $screenshot_index => $screenshot) {
+                $themes[$theme_index]['screenshot'][$screenshot_index] = $this->replace_string($screenshot);
+            }
+        }
+    
+        return $themes;
+    }
+
+    /**
+     * Replace domain in featured thumbnail box
+     */
+    function thumbnail($html) {
+        return $this->replace_string($html);
     }
 
     /**
@@ -43,6 +89,13 @@ class MultipleDomains {
             define('AMBIENT_DOMAINS', explode(',', $domains_string));
         }
     }
+
+    /**
+     * Replaces original domain with new
+     */
+    private function replace_string($string) {
+        return str_replace(AMBIENT_DOMAINS, AMBIENT_DOMAIN, $string);
+    }
 }
 
 /**
@@ -54,38 +107,3 @@ function setup() {
     $controller = new MultipleDomains();
     $controller->init();
 }
-
-function buffer_start($buffer) {
-    return str_replace(AMBIENT_DOMAINS, AMBIENT_DOMAIN, $buffer);
-}
-
-function buffer_end() {
-    ob_end_flush();
-}
-
-/**
- * Replace domain in media gallery
- */
-add_filter('wp_get_attachment_url', function ($url) {
-    return str_replace(AMBIENT_DOMAINS, AMBIENT_DOMAIN, $url);
-});
-
-/**
- * Replace domain in themes gallery
- */
-add_filter('wp_prepare_themes_for_js', function ($themes) {
-    foreach ($themes as $theme_index => $theme) {
-        foreach ($theme['screenshot'] as $screenshot_index => $screenshot) {
-            $themes[$theme_index]['screenshot'][$screenshot_index] = str_replace(AMBIENT_DOMAINS, AMBIENT_DOMAIN, $screenshot);
-        }
-    }
-
-    return $themes;
-});
-
-/**
- * Replace domain in featured thumbnail box
- */
-add_filter('admin_post_thumbnail_html', function ($html) {
-    return str_replace(AMBIENT_DOMAINS, AMBIENT_DOMAIN, $html);
-});
